a2b55ad9c62cba5a1efa3d923a1773e8bbb92c96
KRI3G <150230145+KRI3G@users.noreply.github.com>
2024-05-06 22:33:31 -0500

    Replaced centurion.bash

--- centurion.bash
+++ centurion.bash
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+FORWARDPORT=7700
+ROMULUSLAN=127.0.0.1
+
+if [[ "${1,,}" == "forward" ]]; then 
+	echo "$2 $3 $4" | nc $ROMULUSLAN $FORWARDPORT -q 1 -v
+	echo "Sent forward command to target $2:$3 on Romulus port $4"
+elif [[ "${1,,}" == "exit" ]]; then
+	echo "EXIT" | nc $ROMULUSLAN $FORWARDPORT -q 1 -v
+	echo "Sent 'EXIT' command"
+elif [[ "${1,,}" == "status" ]]; then
+	echo "STATUS" | nc $ROMULUSLAN $FORWARDPORT -q 1 -v
+	echo "Sent 'STATUS' command"
+fi
